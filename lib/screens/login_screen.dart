218b8e8e44061d48f09285bcea167ce0f8b20eaa
Vipin Negi <vipin20823@gmail.com>
2024-06-23 12:16:49 +0530

    modified

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -7,40 +7,75 @@ import 'dashboard_screen.dart';
 
 
 class LoginScreen extends StatelessWidget {
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Login')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              decoration: InputDecoration(labelText: 'Email'),
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true,
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => DashboardScreen()),
-                );
-              },
-              child: Text('Login'),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => SignupScreen()),
-                );
-              },
-              child: Text('Sign Up'),
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                  decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(8.0),
+                          bottomLeft: Radius.circular(12.0),
+                          bottomRight: Radius.circular(16.0),
+                          topRight: Radius.circular(12.0)
+                      )
+                  ),
+                  padding: EdgeInsets.only(left: 8, right: 16.0, top: 8, bottom: 16),
+                  child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),
+                      child: Image.asset('assets/images/icon.png', width: 100, height: 100,))),
+              SizedBox(height: 20),
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                  hintText: 'abc@gmail.com',
+                    contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 16.0)
+                ),
+              ),
+              TextField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                    hintText: '********',
+                    contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 16.0)
+                ),
+                obscureText: true,
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if(_emailController.text.isEmpty || _passwordController.text.isEmpty){
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Invalid Credentials.')));
+                    return;
+                  }
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => DashboardScreen()),
+                  );
+                },
+                child: Text('Login'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => SignupScreen()),
+                  );
+                },
+                child: Text('Sign Up'),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,5 +1,6 @@
 import 'package:basics_of_flutter/screens/dashboard_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 
 class SignupScreen extends StatelessWidget {
@@ -10,37 +11,84 @@ class SignupScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Sign Up')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _userNameController,
-              decoration: InputDecoration(labelText: 'Username'),
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Container(
+                        decoration: BoxDecoration(
+                          color: Colors.green,
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(8.0),
+                              bottomLeft: Radius.circular(12.0),
+                              bottomRight: Radius.circular(16.0),
+                              topRight: Radius.circular(12.0)
+                          )
+                        ),
+                        padding: EdgeInsets.only(left: 8, right: 16.0, top: 8, bottom: 16),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(8.0),
+                            child: Image.asset('assets/images/icon.png', width: 100, height: 100,))),
+                    SizedBox(height: 20),
+                    TextField(
+                      controller: _userNameController,
+                      decoration: InputDecoration(
+                          labelText: 'Username',
+                          border: OutlineInputBorder(),
+                          hintText: 'abc145',
+                      ),
+                    ),
+                    TextField(
+                      controller: _emailController,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        border: OutlineInputBorder(),
+                        hintText: 'abc@gmail.com',
+                      ),
+                    ),
+                    TextField(
+                      controller: _passwordController,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        border: OutlineInputBorder(),
+                        hintText: '********',
+                      ),
+                      obscureText: true,
+                    ),
+                  ],
+                ),
+              ),
             ),
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(labelText: 'Email'),
-            ),
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true,
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
+          ),
+
+          SizedBox(height: 20),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 16.0),
+            color: Colors.blue,
+            height: 50,
+            child: InkWell(
+              onTap: () {
                 if(_emailController.text.isEmpty || _passwordController.text.isEmpty || _userNameController.text.isEmpty){
                   ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Input all fields!!!')));
                   return;
                 }
                 Navigator.push(context, MaterialPageRoute(builder: (context) => DashboardScreen(userName: _userNameController.text,),));
               },
-              child: Text('Sign Up'),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('SignUp', style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),)
+                ],
+              ),
             ),
-          ],
-        ),
+          ),
+          SizedBox(height: 20),
+
+        ],
       ),
     );
   }
